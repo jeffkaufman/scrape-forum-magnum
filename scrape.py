8e90b22041d1d546c45eab778c330f1498fecd4f
Jeff Kaufman <jeff@jefftk.com>
2023-10-05 11:38:57 -0400

    Since scraping comments with before/after doesn't work, switch to scraping them by postId
    See https://github.com/ForumMagnum/ForumMagnum/issues/7950

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+import glob
 import requests
 import datetime
 
@@ -27,6 +28,22 @@ queries = {
     "comments": """
 {
   comments(input: {terms: {limit: %s, after: "%s", before: "%s"}}){
+    results {
+      _id
+      postedAt
+      postId
+      htmlBody
+      parentCommentId
+      user {
+        username
+      }
+    }
+  }
+}
+""",
+    "post_comments": """
+{
+  comments(input: {terms: {view: "postCommentsOld", postId: "%s"}}){
     results {
       _id
       postedAt
@@ -86,6 +103,33 @@ def save_results(server, query_name, after, before, results):
     with open(fname(server, query_name, after, before), "w") as outf:
         json.dump(results, outf)
 
+def fetch_single_post_comments(server, query_name, post_id):
+    populated_query = queries[query_name] % (
+        post_id)
+    r = requests.post(
+        endpoints[server],
+        json={"query": populated_query},
+        headers={"User-Agent": "jefftk"},
+    )
+    r.raise_for_status()
+    return r.json()["data"]["comments"]["results"]
+
+def fetch_post_comments(server, query_name):
+    post_ids = {}
+    for json_fname in glob.glob("%s/posts/*.json" % server):
+        with open(json_fname) as inf:
+            for post in json.load(inf):
+                post_ids[post["_id"]] = post["title"]
+    for post_id, title in sorted(post_ids.items()):
+        post_comments_fname = "%s/%s/%s.json" % (
+            server, query_name, post_id)
+        if os.path.exists(post_comments_fname):
+            continue
+        print("%s (%s)..." % (title, post_id))
+        r = fetch_single_post_comments(server, query_name, post_id)
+        with open(post_comments_fname, "w") as outf:
+            json.dump(r, outf)
+
 if __name__ == "__main__":
     server, query_name = sys.argv[1:]
 
@@ -99,4 +143,9 @@ if __name__ == "__main__":
     except FileExistsError:
         pass
 
-    fetch_all(server, query_name)
+    if query_name in ["posts", "comments"]:
+        fetch_all(server, query_name)
+    elif query_name == "post_comments":
+        fetch_post_comments(server, query_name)
+    else:
+        raise Exception("Unknown query %s" % query_name)
